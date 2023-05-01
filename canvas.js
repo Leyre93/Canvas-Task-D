bced5c0b8dccd3253df1f52d077940607f66e39b
leyre93 <leyreemc93@gmail.com>
2023-05-01 22:57:14 +0100

    Changed colours of rectangle and circle

--- canvas.js
+++ canvas.js
@@ -3,12 +3,12 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 // Draw a rectangle
-ctx.fillStyle = 'red';
+ctx.fillStyle = 'purple';
 ctx.fillRect(50, 50, 100, 50);
 
 // Draw a circle
 ctx.beginPath();
 ctx.arc(200, 100, 50, 0, 2 * Math.PI);
-ctx.fillStyle = 'blue';
+ctx.fillStyle = 'black';
 ctx.fill();
 ctx.stroke();
